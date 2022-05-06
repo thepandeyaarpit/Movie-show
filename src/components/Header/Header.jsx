2ab04ae5ec70a16121c4d78ad62e62a6464aee74
Arpit <69616138+thepandeyaarpit@users.noreply.github.com>
2022-05-06 17:20:52 +0530

    chnge

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 import './Header.css';
 const Header = () => {
   return (
-    <span onClick={() => window.scroll(0, 0)} className='header'>🎥  Review.ME🎥 </span>
+    <>
+      {/* <span onClick={() => window.scroll(0, 0)} className='header'>🎥  Review.ME🎥 </span> */}
+      {/* <span onClick={() => window.scroll(0, 0)} className='header'> Review.ME </span> */}
+      <div className='main'>
+        <div className='mainone'>
+          Review.me
+        </div>
+        <div className='maintwo'>
+          login
+        </div>
+      </div>
+    </>
   )
 }
 
